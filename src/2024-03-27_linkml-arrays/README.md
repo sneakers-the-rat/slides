05528591932a187c20c3d13b176347ace0c4a1fe
sneakers-the-rat <sneakers-the-rat@protonmail.com>
2024-03-27 21:59:56 -0700

    gitignore

---
+++ src/2024-03-27_linkml-arrays/README.md
@@ -0,0 +1,21 @@
+# 2024-03-27_linkml-arrays
+
+[![PyPI - Version](https://img.shields.io/pypi/v/2024-03-27-linkml-arrays.svg)](https://pypi.org/project/2024-03-27-linkml-arrays)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/2024-03-27-linkml-arrays.svg)](https://pypi.org/project/2024-03-27-linkml-arrays)
+
+-----
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [License](#license)
+
+## Installation
+
+```console
+pip install 2024-03-27-linkml-arrays
+```
+
+## License
+
+`2024-03-27-linkml-arrays` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ src/2024-03-27_linkml-arrays/slides/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "2024-03-27: LinkML Arrays"
+copyright = "2024, jonny saunders"
+author = "jonny saunders"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["myst_parser", "sphinx_design", "sphinxcontrib.mermaid"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_css_files = ["main.css"]
+
+html_baseurl = "/slides/2024-03-27_linkml-arrays"
+pygments_dark_style = "github-dark"
